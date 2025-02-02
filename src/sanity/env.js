e6decba6147e95482c652bb37600d326dc1b0677
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2025-02-02 15:34:27 -0500

    Update sanity lib/client & import env variable

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -3,3 +3,4 @@ export const apiVersion =
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const sanityToken = process.env.SANITY_API_READ_TOKEN;
