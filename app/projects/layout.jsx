62d7d893d30577c1f5a11ebe291a74edc7aef20e
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2025-03-27 14:52:38 -0400

    Add Logo image along with component && Made updates to Navbar, ProjectList Components

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -1,9 +1,7 @@
 export default function AboutLayout({ children }) {
   return (
     <section className=''>
-      <div className='inline-block max-w-lg text-center justify-center'>
-        {children}
-      </div>
+      <div className='inline-block justify-center'>{children}</div>
     </section>
   );
 }
